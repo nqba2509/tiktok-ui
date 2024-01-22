a358f5372dc1c9044d1485c18d9d7d78529dbbd6
nqba2509 <nqba2509@gmail.com>
2024-01-22 09:30:38 +0700

    tối ưu Header

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -41,6 +41,25 @@ function Menu({
     });
   };
 
+  const renderResult = (attrs) => (
+    <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
+      <PopperWrapper className={cx("menu-popper")}>
+        {history.length > 1 && (
+          <Header
+            title={currentMenu.title}
+            onBack={() => {
+              console.log(history);
+              setHistory((prev) => prev.slice(0, history.length - 1));
+            }}
+          />
+        )}
+        <div className={cx('menu-scroll')}>{renderItems()}</div>
+      </PopperWrapper>
+    </div>
+  )
+
+  const handleReset = () => setHistory((prev) => prev.slice(0, 1))
+
   return (
     <Tippy
       interactive
@@ -48,23 +67,8 @@ function Menu({
       offset={[12, 8]}
       hideOnClick={hideOnClick}
       placement="bottom-end"
-      render={(attrs) => (
-        <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
-          <PopperWrapper className={cx("menu-popper")}>
-            {history.length > 1 && (
-              <Header
-                title={currentMenu.title}
-                onBack={() => {
-                  console.log(history);
-                  setHistory((prev) => prev.slice(0, history.length - 1));
-                }}
-              />
-            )}
-            <div className={cx('menu-scroll')}>{renderItems()}</div>
-          </PopperWrapper>
-        </div>
-      )}
-      onHide={() => setHistory((prev) => prev.slice(0, 1))}
+      render={renderResult}
+      onHide={handleReset}
     >
       {children}
     </Tippy>
--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -20,11 +20,11 @@ function Search() {
   const [searchResult, setSearchResult] = useState([]);
   const [showResult, setShowResult] = useState(true);
   const [loading, setLoading] = useState(false);
-  const debounced = useDebounce(searchValue, 500);
+  const debouncedValue = useDebounce(searchValue, 500);
   const inputRef = useRef();
 
   useEffect(() => {
-    if (!debounced.trim()) {
+    if (!debouncedValue.trim()) {
       setSearchResult([]);
       return;
     }
@@ -34,13 +34,13 @@ function Search() {
     const fetchApi = async () => {
       setLoading(true);
 
-      const result = await searchServices.search(debounced);
+      const result = await searchServices.search(debouncedValue);
 
       setSearchResult(result);
       setLoading(false);
     };
     fetchApi()
-  }, [debounced]);
+  }, [debouncedValue]);
 
   const handleClear = () => {
     setSearchValue("");
